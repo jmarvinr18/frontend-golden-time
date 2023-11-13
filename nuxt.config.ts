0742e2ec33768a2e7a61b2bdf6cf0dbea8544b0c
marvin <jmarvin.ramoda@gmail.com>
2023-11-13 19:06:48 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'node-server'
+    preset: 'node-server',
+    output: {
+      publicDir: 'dist', // This setting can avoid that the packed dist file is a soft connection
+    },
   },
   app: {
     head: {
