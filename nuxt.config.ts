1917cf359369e486b3a6b08606ea73adad7b2ff1
marvin <jmarvin.ramoda@gmail.com>
2023-11-13 19:29:02 +0800

    static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'node-server',
-  },
   app: {
     head: {
       title: 'Golden Time Food Suplement',
