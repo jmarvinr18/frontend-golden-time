0a63e5e5f6de16d4b5cd0272829f5551785e7581
Arya Saputra <arya@xctuality.com>
2023-11-21 13:06:25 +0700

    added quill as an engine for blog post editor, added gif library for blog editor, added image uploader for blog editor, added mobile version of header, added mobile version of hero section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,10 @@ export default defineNuxtConfig({
         },
         {
           rel: 'stylesheet',
+          href: 'https://cdn.quilljs.com/1.3.6/quill.bubble.css'
+        },
+        {
+          rel: 'stylesheet',
           href: '/css/style.css'
         },
         {
@@ -35,6 +39,10 @@ export default defineNuxtConfig({
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
           type: 'text/javascript'
+        },
+        {
+          src: 'https://cdn.quilljs.com/1.3.6/quill.js',
+          body: true
         }
       ]
     }
