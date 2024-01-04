abfd915c1888e8460bba8134c57ec4f7213c581b
marvin <jmarvin.ramoda@gmail.com>
2024-01-04 21:58:55 +0800

    adjusted search page responsiveness

--- components/Supplement/GSupplementSearchCard.vue
+++ components/Supplement/GSupplementSearchCard.vue
@@ -1,7 +1,5 @@
 <template>
-    <ais-instant-search index-name="supplements" :search-client="search" class="is-desktop w-100 rounded-lg py-5 mt-4 bg-white border border-2" placeholder="Type a keyword...">
-                    
-               
+    <ais-instant-search index-name="supplements" :search-client="search" class="is-desktop w-100 rounded-lg py-5 mt-4 bg-white border border-2">
         <!-- SEARCH BOX AND FILTER -->
         <div class="container py-4">
             <div class="input-group mb-3 rounded-lg border border-dark w-75 mx-auto">
@@ -30,24 +28,34 @@
 
      </ais-instant-search>   
      <!-- SEARCH BOX AND FILTER -->
-     <div class="is-mobile container" style="margin-bottom:-90px">
-        <div class="input-group mb-3 rounded-pill overflow-hidden border border-dark w-100 mx-auto gl-shadow" >
-            <input type="text" class="form-control border-0" placeholder="Type a keyword..." aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text bg-dark text-light px-4 border-0" id="basic-addon2" style="border-radius:0 0 0 30px">
-                <i class="bi bi-sort-up"></i>
-            </span>
+
+     <ais-instant-search index-name="supplements" :search-client="search">
+        <div class="is-mobile container" style="margin-bottom:-90px">
+            <div class="input-group mb-3 rounded-pill overflow-hidden border border-dark w-100 mx-auto gl-shadow" >
+                
+                <ais-search-box :class-names="{'ais-SearchBox-input': 'search_box'}" class="form-control border-0" placeholder="Search here" />
+                <span class="input-group-text bg-dark text-light px-4 border-0" id="basic-addon2" style="border-radius:0 0 0 30px">
+                    <i class="bi bi-sort-up"></i>
+                </span>
+            </div>
+            <div class="gl-search-filter-category d-flex justify-content-between w-100 mx-auto px-4">
+                <UtilsGButtonFilter v-for="(opt,index) in filterOpts" :title="opt.title" :checked="filters.type.includes(opt.value)" @on-click="toggleFilter(opt.value)"></UtilsGButtonFilter>
+            </div>
         </div>
-        <div class="gl-search-filter-category d-flex justify-content-between w-100 mx-auto px-4">
-            <UtilsGButtonFilter v-for="(opt,index) in filterOpts" :title="opt.title" :checked="filters.type.includes(opt.value)" @on-click="toggleFilter(opt.value)"></UtilsGButtonFilter>
+        <div class="is-mobile w-100 rounded-lg py-5 mt-4 bg-white border border-2">
+            <!-- SEARCH RESULT -->
+            <div class="container mx-auto mt-5">
+                <ais-hits :class-names="{'ais-Hits-item': 'searchHitItems'}" :escapeHTML="true">
+                    <template v-slot:item="{ item }">
+                        <SupplementGSupplementSearchItem :supplement="item" ></SupplementGSupplementSearchItem>
+                    </template>
+
+                </ais-hits>                
+                <!-- <SupplementGSupplementSearchItem :supplement="supplement" v-for="(supplement, i) in allSupplements" :key="supplement.id"></SupplementGSupplementSearchItem>
+                <UtilsGLoadMore></UtilsGLoadMore> -->
+            </div>
         </div>
-    </div>
-    <div class="is-mobile w-100 rounded-lg py-5 mt-4 bg-white border border-2">
-        <!-- SEARCH RESULT -->
-        <div class="container mx-auto mt-5">
-            <SupplementGSupplementSearchItem :supplement="supplement" v-for="(supplement, i) in allSupplements" :key="supplement.id"></SupplementGSupplementSearchItem>
-            <UtilsGLoadMore></UtilsGLoadMore>
-        </div>
-    </div>
+    </ais-instant-search>
 </template>
 
 <script lang="ts">
