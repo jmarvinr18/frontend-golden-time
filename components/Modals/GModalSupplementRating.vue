cc457516f52852917941c962962d407e29ee7be2
Arya Saputra <arya@xctuality.com>
2024-02-04 13:32:25 +0700

    added fixes for login_page.pptx from client

--- components/Modals/GModalSupplementRating.vue
+++ components/Modals/GModalSupplementRating.vue
@@ -1,14 +1,14 @@
 <template>
-    <ModalsGModal title="Post a rating" :id="`rating-modal-${type}`" :submitBtn="true" @on-submit="submitNow" size="md">
+    <ModalsGModal title="" :id="`rating-modal-${type}`" :submitBtn="true" @on-submit="submitNow" size="md">
         <div class="text-center">
-            <div class="h5 mb-4">Rate the {{ type }}</div>
+            <div class="h5 mb-4">{{ type=='effect'? $t('EffectSupplementLabel'):$t('TasteSupplementLabel') }}{{ $t('RateLabel') }}</div>
             <label v-for="star in 5" class="cursor-pointer mx-1" :for="'star-'+type+'-'+star">
                 <i v-if="type=='taste'" class="bi h3 text-grad-1" :class="rates >= star? 'bi-star-fill '+ type:'bi-star '+ type"></i>
                 <i v-else class="bi h2 text-grad-2" :class="rates >= star? 'bi-star-fill '+ type:'bi-star '+ type"></i>
                 <input :id="'star-'+type+'-'+star" :value="star" :name="`star-rating-${type}`" type="radio" class="radio-star visibility-hidden" v-model="rates" />
             </label>
 
-            <div class="text-muted text-italic mt-3">{{ msg }}</div>
+            <!-- <div class="text-muted text-italic mt-3">{{ msg }}</div> -->
         </div>
     </ModalsGModal>
 </template>
