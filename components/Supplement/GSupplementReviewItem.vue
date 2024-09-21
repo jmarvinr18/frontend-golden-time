3c25e61200c4b5a8b408f5ac774193a4ec59fd6d
Marvin <josemarvin.ra@reliasourcing.com>
2024-09-21 09:34:44 +0800

    fixed the responsiveness of supplements page

--- components/Supplement/GSupplementReviewItem.vue
+++ components/Supplement/GSupplementReviewItem.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="is-desktop g-review-item">
         <div class="g-review-item-media">
-            <img class="g-review-item-image g-shadow w-100 rounded object-fit-cover" :src="supplement?.image" style="height: 190px;" />
+            <img class="g-review-item-image g-shadow rounded" :src="supplement?.image" />
         </div>
-        <div class="g-review-item-description w-75">
+        <div class="g-review-item-description w-100">
             <div class="g-review-item-head">
+                <div>
+                    <div class="h4"> {{ supplement?.name }}</div>
 
-                <div class="h4"> {{ supplement?.name }}</div>
-                <nuxt-link v-if="isContentOwner" :to="`/supplement/edit/${supplement?.id}`" class="btn bg-none btn-sm rounded-pill w-50 f12">
+                    <NuxtLink class="g-review-item-owner w-100 d-flex align-items-center text-decoration-none text-dark" :to="`/users/${supplement?.user?.id}`">
+                        <img class="rounded-circle me-2" :src="getProfileImage" style="height: 20px; width: 20px;" />
+                        <div class="f12">{{ supplement?.user?.name }}</div>
+                    </NuxtLink>                    
+                </div>
+                
+                <nuxt-link v-if="isContentOwner" :to="`/supplement/edit/${supplement?.id}`" class="btn bg-none btn-sm rounded-pill w-25 f12">
                     <i class="bi bi-pencil me-2"></i>
                     {{ $t('EditInformation') }}
                 </nuxt-link>
@@ -17,16 +24,32 @@
                 <div class="w-100 f14 lh-lg">
                     <div>{{ $t('TasteLabel') }}: {{ supplement?.flavor }}</div>
                     <div>{{ $t('PriceLabel') }}: {{ price }} </div>
-
-                 
                 </div>
-                <NuxtLink class="g-review-item-owner w-100 d-flex align-items-center text-decoration-none text-dark" :to="`/users/${supplement?.user?.id}`">
-                        <img class="rounded-circle me-2" :src="getProfileImage" style="height: 30px; width: 30px;" />
-                        <div class="f12">{{ supplement?.user?.name }}</div>
-                </NuxtLink>                    
+                <div v-if="isDesktop" class="g-review-item-actions">
+                    <div v-if="isAuthenticated">
+                        <button @click="toggleHasDrank(supplement?.id)" class="btn btn-primary btn-sm rounded-pill py-2 f12 d-flex justify-content-center gap-3">
+                            <i v-if="supplement?.has_user_drank_the_supplement" class="bi bi-hand-thumbs-up-fill"></i>
+                            <span class="align-self-center">
+                                <i class="bi bi-people me-2"></i>
+                                {{ $t('MySupplementRegistration', { count: supplement?.users_who_drank_the_supplement_count}) }}
+                            </span>
+                        </button>
+                        <button @click="toggleDrinkWish(supplement?.id)" class="btn btn-outline-secondary btn-sm rounded-pill py-2 f12 d-flex justify-content-center gap-3">
+                            <i v-if="supplement?.on_users_wishlist" class="bi bi-hand-thumbs-up-fill"></i>
+                            <span class="align-self-center">
+                                <i class="bi bi-person-heart me-2"></i>
+                                {{ $t('PeopleWhoWantToDrink', { count: supplement?.user_supplement_wish_count}) }}
+                            </span>                         
+                        </button>
+                    </div>
+                    <div v-else>
+                        <NuxtLink :to="`/login?ref=${route.fullPath}`" class="btn btn-primary btn-sm rounded-pill py-2 f14">{{ $t("Login") }} </NuxtLink>
+                    </div>
+                </div>                   
             </div>
-            <div>
-                <div class="g-review-item-actions">
+        </div>
+    </div>
+    <div v-if="!isDesktop" class="g-review-item-actions">
                     <div class="d-flex gap-3 mt-4" v-if="isAuthenticated">
                         <button @click="toggleHasDrank(supplement?.id)" class="btn btn-primary btn-sm rounded-pill py-2 f14 d-flex justify-content-center gap-3">
                             <i v-if="supplement?.has_user_drank_the_supplement" class="bi bi-hand-thumbs-up-fill"></i>
@@ -46,11 +69,7 @@
                     <div v-else>
                         <NuxtLink :to="`/login?ref=${route.fullPath}`" class="btn btn-primary btn-sm rounded-pill py-2 f14">{{ $t("Login") }} </NuxtLink>
                     </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
+                </div>    
     <div class="is-mobile g-review-item d-flex flex-wrap">
         <div class="w-100 text-end">
             <nuxt-link v-if="isContentOwner" :to="`/supplement/edit/${supplement?.id}`" class="btn bg-none btn-sm rounded-pill w-50 f12">
@@ -116,6 +135,7 @@ export default defineComponent({
         var { userData, isAuthenticated } = storeToRefs(authStore)
         var supplementStore = useSupplementStore()
         var price = ref()
+        var isDesktop = ref(true)
 
         var toggleDrinkWish = (id: string) => {
             supplementStore.toggleDrinkWish(id, false)
@@ -139,9 +159,15 @@ export default defineComponent({
 
         var convertToCurrency = (price: string) => {
             console.log("TYPE_OF: ", typeof(parseFloat(price)))
-            // if(typeof)
-            // return n(parseFloat(price), 'currency', 'ja')
         }
+
+        onMounted(() => {
+            isDesktop.value = window.innerWidth > 600 ? true : false
+            window.addEventListener('resize', () => {
+                console.log("SCREEN WIDTH: ", window.innerWidth > 600 ? true : false)
+                isDesktop.value = window.innerWidth > 600 ? true : false
+            })               
+        })
         
         return {
             route,
@@ -151,7 +177,8 @@ export default defineComponent({
             toggleDrinkWish,
             toggleHasDrank,
             convertToCurrency,
-            price
+            price,
+            isDesktop
         }
     }  
 })
@@ -170,9 +197,35 @@ export default defineComponent({
     justify-content: space-between;
 }
 .g-review-item-actions{
+    display: flex;
     width: 100%;
+    flex-direction: column;
+    gap: 1rem;
 }
 .g-review-item-actions > div > button {
-    width: 15rem;
+    width: 13rem;
+}
+.g-review-item-actions > div {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center
+}
+.g-review-item-image{
+    height: 11rem;
+    width: 11rem;
+    object-fit: cover
+}
+
+@media only screen and (max-width: 600px) {
+    .g-review-item-actions > div > button {
+        width: 14rem;
+    }    
+    .g-review-item-actions > div {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        justify-content: center
+    }    
 }
 </style>
